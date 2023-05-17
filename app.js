37e92acc926c3ad6754ad5a85bdd0b54908e7afc
Aleksandar Kosanovic <aleksandar.kosanovic88@gmail.com>
2023-05-17 12:38:46 +0200

    Express install. Middleware and routing

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const http = require("http");
+const express = require("express");
 
-const routes = require("./routes");
+// it automatically sets up certain way of handling incoming requests that is a key characteristic of express.js
+const app = express();
 
-const server = http.createServer(routes.handler);
+// use allows us to add new middleware
+app.use('/', (req, res, next) => {
+    console.log('This always run');
+    next(); // allows the request to continue to next middleware
+});
 
-server.listen(3000);
+app.use('/product', (req, res, next) => {
+    res.send('<h1>This is product page</h1>')
+});
+app.use('/', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>Welcome to express node</h1>')
+});
+
+
+
+// const server = http.createServer(app);
+// server.listen(3000);
+// this line replaces http create server
+app.listen(3000);
+
